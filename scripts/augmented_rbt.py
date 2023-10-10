fa3258695dc5661663a1737bfadcc2fa259d7213
Alberto Del Buono Paolini <alberto.delbuonopaolini@gmail.com>
2023-10-10 11:39:53 +0000

    Added type annotations to rbt

--- scripts/augmented_rbt.py
+++ scripts/augmented_rbt.py
@@ -3,11 +3,16 @@ from timeit import default_timer as timer
 import numpy as np
 import utils
 import statistics
+from enum import Enum
+
+class Color(Enum):
+    RED = 1
+    BLACK = 2
 
 class RedBlackTreeNode:
-    def __init__(self, value, color, size):
+    def __init__(self, value: int, color: Color, size: int):
         self.value = value
-        self.color = color  # "R" per rosso, "B" per nero
+        self.color = color
         self.size = size
         self.left = None
         self.right = None
@@ -16,12 +21,12 @@ class RedBlackTree:
     def __init__(self):
         self.root = None
 
-    def insert(self, value):
+    def insert(self, value: int):
         self.root = self._insert_recursive(self.root, value)
 
-    def _insert_recursive(self, root, value):
+    def _insert_recursive(self, root: RedBlackTreeNode | None, value: int):
         if root is None:
-            return RedBlackTreeNode(value, "R", 1)
+            return RedBlackTreeNode(value, Color.RED, 1)
 
         if value < root.value:
             root.left = self._insert_recursive(root.left, value)
@@ -39,10 +44,10 @@ class RedBlackTree:
         root.size = 1 + self.size(root.left) + self.size(root.right)
         return root
 
-    def os_select(self, k):
+    def os_select(self, k: int):
         return self._os_select_recursive(self.root, k)
 
-    def _os_select_recursive(self, root, k):
+    def _os_select_recursive(self, root: RedBlackTreeNode | None, k: int):
         if root is None:
             return None
 
@@ -54,10 +59,10 @@ class RedBlackTree:
         else:
             return self._os_select_recursive(root.right, k - left_size - 1)
 
-    def os_rank(self, value):
+    def os_rank(self, value: int):
         return self._os_rank_recursive(self.root, value)
 
-    def _os_rank_recursive(self, root, value):
+    def _os_rank_recursive(self, root: RedBlackTreeNode | None, value: int):
         if root is None:
             return None
 
@@ -73,43 +78,43 @@ class RedBlackTree:
             else:
                 return None
 
-    def size(self, root):
+    def size(self, root: RedBlackTreeNode | None):
         if root is None:
             return 0
         return root.size
 
-    def is_red(self, node):
+    def is_red(self, node: RedBlackTreeNode | None):
         if node is None:
             return False
-        return node.color == "R"
+        return node.color == Color.RED
 
-    def rotate_left(self, h):
+    def rotate_left(self, h: RedBlackTreeNode):
         x = h.right
         h.right = x.left
         x.left = h
         x.color = h.color
-        h.color = "R"
+        h.color = Color.RED
         x.size = h.size
         h.size = 1 + self.size(h.left) + self.size(h.right)
         return x
 
-    def rotate_right(self, h):
+    def rotate_right(self, h: RedBlackTreeNode):
         x = h.left
         h.left = x.right
         x.right = h
         x.color = h.color
-        h.color = "R"
+        h.color = Color.RED
         x.size = h.size
         h.size = 1 + self.size(h.left) + self.size(h.right)
         return x
 
-    def flip_colors(self, h):
-        h.color = "R"
-        h.left.color = "B"
-        h.right.color = "B"
+    def flip_colors(self, h: RedBlackTreeNode):
+        h.color = Color.RED
+        h.left.color = Color.BLACK
+        h.right.color = Color.BLACK
 
-# Funzione per eseguire il test di inserimento
-def test_rn_insertion(red_black_tree, size):
+# Funzione per eseguire l'inserimento dei dati
+def test_rn_insertion(red_black_tree: RedBlackTree, size: int):
     data = [random.randint(1, 1000) for _ in range(size)]
     start_time = timer()
     for item in data:
@@ -118,8 +123,8 @@ def test_rn_insertion(red_black_tree, size):
 
     return [end_time - start_time, data]
 
-# Funzione per eseguire il test di OS-select
-def test_rn_os_select(red_black_tree, size):
+# Funzione di test per OS-select
+def test_rn_os_select(red_black_tree: RedBlackTree, size: int):
     start_time = timer()
     for k in [random.randint(1, size) for _ in range(size)]:
         red_black_tree.os_select(k)
@@ -127,8 +132,8 @@ def test_rn_os_select(red_black_tree, size):
 
     return end_time - start_time
 
-# Funzione per eseguire il test di OS-rank
-def test_rn_os_rank(red_black_tree, data):
+# Funzione di test per OS-rank
+def test_rn_os_rank(red_black_tree: RedBlackTree, data: list[int]):
     start_time = timer()
     for k in data:
         red_black_tree.os_rank(k)
@@ -136,7 +141,8 @@ def test_rn_os_rank(red_black_tree, data):
 
     return end_time - start_time
 
-def test_augmented_rbt(sizes, iterations):
+# Esegue i test per ogni dimensione con un certo numero di iterazioni
+def test_augmented_rbt(sizes: list[int], iterations: int):
     os_select_times = []
     os_rank_times = []
 
