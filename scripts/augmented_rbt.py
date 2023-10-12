7a5f8d46c72b474ae00697b534165d6c0a4708bf
Alberto Del Buono Paolini <alberto.delbuonopaolini@gmail.com>
2023-10-12 15:48:27 +0000

    Changed some methods to private

--- scripts/augmented_rbt.py
+++ scripts/augmented_rbt.py
@@ -34,14 +34,14 @@ class RedBlackTree:
             root.right = self._insert_recursive(root.right, value)
 
         # Bilancia l'albero
-        if self.is_red(root.right) and not self.is_red(root.left):
-            root = self.rotate_left(root)
-        if root and root.left and self.is_red(root.left) and self.is_red(root.left.left):
-            root = self.rotate_right(root)
-        if root and self.is_red(root.left) and self.is_red(root.right):
-            self.flip_colors(root)
+        if self._is_red(root.right) and not self._is_red(root.left):
+            root = self._rotate_left(root)
+        if root and root.left and self._is_red(root.left) and self._is_red(root.left.left):
+            root = self._rotate_right(root)
+        if root and self._is_red(root.left) and self._is_red(root.right):
+            self._flip_colors(root)
 
-        if root: root.size = 1 + self.size(root.left) + self.size(root.right)
+        if root: root.size = 1 + self._size(root.left) + self._size(root.right)
         return root
 
     def os_select(self, k: int) -> Optional[int]:
@@ -51,7 +51,7 @@ class RedBlackTree:
         if root is None:
             return None
 
-        left_size = self.size(root.left)
+        left_size = self._size(root.left)
         if left_size == k - 1:
             return root.value
         elif left_size > k - 1:
@@ -67,28 +67,28 @@ class RedBlackTree:
             return None
 
         if value == root.value:
-            return self.size(root.left) + 1
+            return self._size(root.left) + 1
 
         if value < root.value:
             return self._os_rank_recursive(root.left, value)
         else:
             right_rank = self._os_rank_recursive(root.right, value)
             if right_rank is not None:
-                return self.size(root.left) + 1 + right_rank
+                return self._size(root.left) + 1 + right_rank
             else:
                 return None
 
-    def size(self, root: Optional[RedBlackTreeNode]) -> int:
+    def _size(self, root: Optional[RedBlackTreeNode]) -> int:
         if root is None:
             return 0
         return root.size
 
-    def is_red(self, node: Optional[RedBlackTreeNode]) -> bool:
+    def _is_red(self, node: Optional[RedBlackTreeNode]) -> bool:
         if node is None:
             return False
         return node.color == Color.RED
 
-    def rotate_left(self, h: RedBlackTreeNode) -> Optional[RedBlackTreeNode]:
+    def _rotate_left(self, h: RedBlackTreeNode) -> Optional[RedBlackTreeNode]:
         if not h.right: return None
         x: RedBlackTreeNode = h.right
         h.right = x.left
@@ -96,10 +96,10 @@ class RedBlackTree:
         x.color = h.color
         h.color = Color.RED
         x.size = h.size
-        h.size = 1 + self.size(h.left) + self.size(h.right)
+        h.size = 1 + self._size(h.left) + self._size(h.right)
         return x
 
-    def rotate_right(self, h: RedBlackTreeNode) -> Optional[RedBlackTreeNode]:
+    def _rotate_right(self, h: RedBlackTreeNode) -> Optional[RedBlackTreeNode]:
         if not h.left: return None
         x: RedBlackTreeNode = h.left
         h.left = x.right
@@ -107,10 +107,10 @@ class RedBlackTree:
         x.color = h.color
         h.color = Color.RED
         x.size = h.size
-        h.size = 1 + self.size(h.left) + self.size(h.right)
+        h.size = 1 + self._size(h.left) + self._size(h.right)
         return x
 
-    def flip_colors(self, h: RedBlackTreeNode):
+    def _flip_colors(self, h: RedBlackTreeNode):
         h.color = Color.RED
         if h.left: h.left.color = Color.BLACK
         if h.right: h.right.color = Color.BLACK
